5dae8f8ad913925d73f531c3fe8f00161a866b9b
danielagmz <161260538+danielagmz@users.noreply.github.com>
2024-05-16 13:56:11 +0200

    modificacion al menu

--- src/Controlador/Menu.java
+++ src/Controlador/Menu.java
@@ -19,8 +19,8 @@ public class Menu {
             System.out.println("3) Llistar tots els partits jugats per un equip amb el seu resultat");
             System.out.println("4) Inserir un nou jugador a un equip");
             System.out.println("5) Traspassar un judador a un altra equip");
-            System.out.println("6) Actualitzar les dades de jugadors");
-            System.out.println("7) Modificar les estadístiques d’un jugador");
+            System.out.println("6) Actualitzar les dades de jugadors despres del partit");
+            System.out.println("7) Modificar les estadístiques d’un jugador a un partit");
             System.out.println("8) Retirar un jugador.");
             System.out.println("9) Canviar nom franquícia d’un equip");
             System.out.println("0) acabar");
